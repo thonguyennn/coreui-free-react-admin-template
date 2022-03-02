b7429c12636641fbdf82337ee2ecf357ab82d804
thonguyenn922 <thonguyenn922@gmail.com>
2022-03-02 11:31:24 +0700

    commit 02032022

--- src/components/AppHeaderDropdown.js
+++ src/components/AppHeaderDropdown.js
@@ -37,14 +37,14 @@ const AppHeaderDropdown = () => {
   return (
     <CDropdown variant="nav-item">
       <CDropdownToggle placement="bottom-end" className="py-0" caret={false}>
-      <div className="c-avatar">
+      {/* <div className="c-avatar">
           <CImage
             src={`${me.avatar || 'avatars/6.jpg'}`}
             className="c-avatar-img"
             alt={`${me.email || 'alt@email.com'}`}
           />
-      </div>
-      {/* <CAvatar src={avatar8} size="md" /> */}
+      </div> */}
+      <CAvatar src={avatar8} size="md" />
       </CDropdownToggle>
       <CDropdownMenu className="pt-0" placement="bottom-end">
         <CDropdownHeader className="bg-light fw-semibold py-2 text-center">Settings</CDropdownHeader>
